1651873bc7415d264d6c60693e4afffcdbe86c23
Henry Maddocks <hmaddocks@me.com>
2025-04-22 08:54:30 +1200

    Add red sphere

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,31 @@ mod point3;
 mod ray;
 mod vec3;
 
+//     vec3 oc = center - r.origin();
+//     auto a = dot(r.direction(), r.direction());
+//     auto b = -2.0 * dot(r.direction(), oc);
+//     auto c = dot(oc, oc) - radius*radius;
+//     auto discriminant = b*b - 4*a*c;
+//     return (discriminant >= 0);
+// }
+
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> bool {
+    let oc = center - ray.origin();
+    let a = ray.direction().dot(ray.direction());
+    let b = -2.0 * ray.direction().dot(&oc);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
-    let unit_direction = r.direction().unit();
-    let a = 0.5 * (unit_direction.y() + 1.0);
-    Color::new(1.0, 1.0, 1.0) * (1.0 - a) + Color::new(0.5, 0.7, 1.0) * a
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        Color::new(1.0, 0.0, 0.0)
+    } else {
+        let unit_direction = r.direction().unit();
+        let a = 0.5 * (unit_direction.y() + 1.0);
+        Color::new(1.0, 1.0, 1.0) * (1.0 - a) + Color::new(0.5, 0.7, 1.0) * a
+    }
 }
 
 fn main() {
@@ -28,12 +49,12 @@ fn main() {
     let view_port_u = Vec3::new(viewport_width, 0.0, 0.0);
     let view_port_v = Vec3::new(0.0, -viewport_height, 0.0);
 
-    let pixel_delta_u = &view_port_u / image_width as f64;
-    let pixel_delta_v = &view_port_v / image_height as f64;
+    let pixel_delta_u = view_port_u / image_width as f64;
+    let pixel_delta_v = view_port_v / image_height as f64;
 
     let viewport_upper_left =
-        camera_center - Vec3::new(0.0, 0.0, focal_length) + &view_port_u / 2.0 - &view_port_v / 2.0;
-    let pixel00_loc = viewport_upper_left + &pixel_delta_u / 2.0 + &pixel_delta_v / 2.0;
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - view_port_u / 2.0 - view_port_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     println!("P3");
     println!("{} {}", image_width, image_height);
