5cd33b031a0f1ba85a3cf77e77604a1093f239aa
Henry Maddocks <hmaddocks@me.com>
2025-04-22 10:47:26 +1200

    Surface normals

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,23 @@ mod vec3;
 //     return (discriminant >= 0);
 // }
 
-fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> Option<f64> {
     let oc = center - ray.origin();
     let a = ray.direction().dot(ray.direction());
     let b = -2.0 * ray.direction().dot(&oc);
     let c = oc.dot(&oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant >= 0.0
+    if discriminant < 0.0 {
+        None
+    } else {
+        Some((-b - discriminant.sqrt()) / (2.0 * a))
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        Color::new(1.0, 0.0, 0.0)
+    if let Some(t) = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+let n: Vec3 = (r.at(t) - Point3::new(0.0, 0.0, -1.0)).unit();
+        Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5
     } else {
         let unit_direction = r.direction().unit();
         let a = 0.5 * (unit_direction.y() + 1.0);
@@ -37,7 +42,7 @@ fn ray_color(r: &Ray) -> Color {
 
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 800;
     let mut image_height = (image_width as f64 / aspect_ratio) as u32;
     image_height = if image_height < 1 { 1 } else { image_height };
 
@@ -46,15 +51,19 @@ fn main() {
     let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
     let camera_center = Point3::new(0.0, 0.0, 0.0);
 
-    let view_port_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let view_port_v = Vec3::new(0.0, -viewport_height, 0.0);
+    let view_port_u = Point3::new(viewport_width, 0.0, 0.0);
+    let view_port_v = Point3::new(0.0, -viewport_height, 0.0);
 
     let pixel_delta_u = view_port_u / image_width as f64;
     let pixel_delta_v = view_port_v / image_height as f64;
 
-    let viewport_upper_left =
-        camera_center - Vec3::new(0.0, 0.0, focal_length) - view_port_u / 2.0 - view_port_v / 2.0;
-    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    let viewport_upper_left_vec = camera_center.as_vec3()
+        - Point3::new(0.0, 0.0, focal_length).as_vec3()
+        - (view_port_u / 2.0).as_vec3()
+        - (view_port_v / 2.0).as_vec3();
+    let viewport_upper_left: Point3 = viewport_upper_left_vec.into();
+
+    let pixel00_loc = viewport_upper_left + 0.5 * pixel_delta_u.as_vec3() + 0.5 * pixel_delta_v.as_vec3();
 
     println!("P3");
     println!("{} {}", image_width, image_height);
@@ -63,8 +72,8 @@ fn main() {
     for j in 0..image_height {
         eprint!("\rScanlines remaining: {}             ", image_height - j);
         for i in 0..image_width {
-            let pixel_center = pixel00_loc + pixel_delta_u * i as f64 + pixel_delta_v * j as f64;
-            let ray_direction: Vec3 = &pixel_center - &camera_center;
+            let pixel_center: Point3 = pixel00_loc + pixel_delta_u * i as f64 + pixel_delta_v * j as f64;
+            let ray_direction = pixel_center - camera_center;
             let r = Ray::new(camera_center, ray_direction);
 
             let pixel_color = ray_color(&r);
--- src/point3.rs
+++ src/point3.rs
@@ -1,5 +1,5 @@
 use crate::vec3::Vec3;
-use std::ops::{Add, Sub};
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Point3(Vec3);
@@ -24,6 +24,17 @@ impl Point3 {
     pub const fn z(&self) -> f64 {
         self.0.z()
     }
+
+    #[inline]
+    pub fn as_vec3(&self) -> Vec3 {
+        self.0
+    }
+}
+
+impl From<Vec3> for Point3 {
+    fn from(value: Vec3) -> Self {
+        Point3(value)
+    }
 }
 
 impl Add<Vec3> for Point3 {
@@ -44,17 +55,21 @@ impl Add<Point3> for Point3 {
     type Output = Point3;
 
     #[inline]
-    fn add(self, _other: Point3) -> Point3 {
-        panic!("Cannot add two Point3 values together: operation is undefined.");
+    fn add(self, other: Point3) -> Point3 {
+        Point3::new(
+            self.x() + other.x(),
+            self.y() + other.y(),
+            self.z() + other.z(),
+        )
     }
 }
 
 impl Sub<Vec3> for Point3 {
-    type Output = Point3;
+    type Output = Vec3;
 
     #[inline]
-    fn sub(self, other: Vec3) -> Point3 {
-        Point3::new(
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3::new(
             self.0.x() - other.x(),
             self.0.y() - other.y(),
             self.0.z() - other.z(),
@@ -63,11 +78,11 @@ impl Sub<Vec3> for Point3 {
 }
 
 // Allow Point3 - Point3 = Vec3
-impl Sub for &Point3 {
+impl Sub for Point3 {
     type Output = Vec3;
 
     #[inline]
-    fn sub(self, other: &Point3) -> Vec3 {
+    fn sub(self, other: Point3) -> Vec3 {
         Vec3::new(
             self.x() - other.x(),
             self.y() - other.y(),
@@ -76,16 +91,70 @@ impl Sub for &Point3 {
     }
 }
 
+impl<'a, 'b> Sub<&'b Point3> for &'a Point3 {
+    type Output = Vec3;
+
+    #[inline]
+    fn sub(self, other: &'b Point3) -> Vec3 {
+        Vec3::new(self.x() - other.x(), self.y() - other.y(), self.z() - other.z())
+    }
+}
+
+// Allow Point3 - &Point3 = Vec3
+impl<'a> Sub<&'a Point3> for Point3 {
+    type Output = Vec3;
+    fn sub(self, other: &'a Point3) -> Vec3 {
+        Vec3::new(self.x() - other.x(), self.y() - other.y(), self.z() - other.z())
+    }
+}
+
+// Allow &Point3 - Point3 = Vec3
+impl<'a> Sub<Point3> for &'a Point3 {
+    type Output = Vec3;
+    fn sub(self, other: Point3) -> Vec3 {
+        Vec3::new(self.x() - other.x(), self.y() - other.y(), self.z() - other.z())
+    }
+}
+
+impl Div<f64> for Point3 {
+    type Output = Point3;
+
+    #[inline]
+    fn div(self, other: f64) -> Point3 {
+        Point3::new(self.x() / other, self.y() / other, self.z() / other)
+    }
+}
+
+impl Mul<Point3> for f64 {
+    type Output = Point3;
+
+    #[inline]
+    fn mul(self, other: Point3) -> Point3 {
+        Point3::new(self * other.x(), self * other.y(), self * other.z())
+    }
+}
+
+impl Mul<f64> for Point3 {
+    type Output = Point3;
+
+    #[inline]
+    fn mul(self, other: f64) -> Point3 {
+        Point3::new(self.x() * other, self.y() * other, self.z() * other)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    #[should_panic(expected = "Cannot add two Point3 values together: operation is undefined.")]
-    fn test_point3_add_point3_panics() {
+    fn test_point3_add_point3() {
         let p1 = Point3::new(1.0, 2.0, 3.0);
         let p2 = Point3::new(4.0, 5.0, 6.0);
-        let _ = p1 + p2;
+        let result = p1 + p2;
+        assert_eq!(result.x(), 5.0);
+        assert_eq!(result.y(), 7.0);
+        assert_eq!(result.z(), 9.0);
     }
 
     #[test]
